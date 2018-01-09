20090a7add86ceef6635fa64039b7a465ade079a
arsalan13 <arsalan7ctech@gmail.com>
2018-01-10 02:08:58 +0500

    template file 5 changes backup

--- template-products-library.php
+++ template-products-library.php
@@ -3,15 +3,134 @@
  * Template Name: Library Page
  */
 get_header();
+$template_type = get_field( "page_template_style" );
+
+	$category_id = ( get_field( 'category' ) != 0 ) ? get_field( 'category' ) : '-'.get_field( 'category' );
+
+	global $post;
+	$args = array (
+	    'cat' => array( $category_id ),
+	    'posts_per_page' => -1, //showposts is deprecated
+	    'fields' => 'ids'
+	);
+	$all_posts = get_posts( $args );
 ?>
 <?php if ( is_active_sidebar('webinar-sidebar') ) : ?>
 	<div class="page-right-sidebar">
 <?php else : ?>
 	<div class="page-full-width">
 <?php endif; ?>
-<div id="primary" class="site-content">
+<div id="primary" class="site-content">	
 	<div id="content" role="main">
 		<article>
+
+		<?php if( !isset( $template_type ) || $template_type == 1 ) : ?>
+
+		<div class="pl-grid-wrapper">
+	<?php 
+		/**** Memeberium  Accessible Posts *****/
+		foreach( $all_posts as $p_id => $post_id ) :	
+
+			if ( memb_hasPostAccess( $post_id ) ) :
+				$class = '';
+			else:
+				$class="no-access grayscale";
+			endif;	
+
+			$product_posts['link'] = get_the_permalink( $post_id ); 
+			$custom_size_attachment_image = wp_get_attachment_image_src( get_post_thumbnail_id( $post_id ), 'product_library_image_size', true ); 
+			$product_posts['img'] = $custom_size_attachment_image[0];
+			$product_posts['title'] = get_the_title( $post_id ); 
+			$post_excerpt = apply_filters( 'the_excerpt', get_post_field('post_excerpt', $post_id) );
+			$product_posts['excerpt'] = ( !empty( $post_excerpt ) ) 
+			? $post_excerpt
+			: '';	
+	?>
+				<div class="grid-course pl-col-14 pl-3-col">
+					<div class="pl-border">
+						<div class="pl-box">
+							<?php if( $product_posts['img'] != '' ) : ?>
+							<div class="featured-image">
+								<a href="<?php echo $product_posts['link']; ?>">						
+									<img src="<?php echo $product_posts['img']; ?>" class="uo-grid-featured-image <?php echo $class; ?>">						
+								</a>
+							</div>
+							<?php endif; ?>
+							<div class="course-info-holder">
+								<div class="course-title"><?php echo $product_posts['title']; ?></div>
+										<div class="content_desc"><?php echo wpautop( $product_posts['excerpt']); ?></div>
+							</div>
+							<a href="<?php echo $product_posts['link']; ?>"><div class="view-course-outline">View More</div></a>					
+							<!-- <div class="price-credit-btns">
+								<a href="<?php echo get_field( "sales_url", $p_id ); ?>">
+									<div class="view-course-outline" >Price: <?php echo get_field( "post_price", $p_id );  ?></div>
+								</a>	
+								<a href="<?php echo home_url('/credit-sale/'); ?>">
+									<div class="view-course-outline" >Credit: <?php echo get_field( "post_credit", $p_id );  ?></div>
+								</a>		
+							</div>		 -->				
+						</div>					
+					</div>
+				</div>	
+			<?php
+				endforeach;
+				wp_reset_postdata();
+			?>	
+			</div>
+	<?php elseif( $template_type == 2 ): ?>
+	
+		<div class="pl-grid-wrapper">
+	<?php 
+		/**** Memeberium  Accessible Posts *****/
+		foreach( $all_posts as $p_id => $post_id ) :	
+
+			if ( memb_hasPostAccess( $post_id ) ) :
+
+			$product_posts['link'] = get_the_permalink( $post_id ); 
+			$custom_size_attachment_image = wp_get_attachment_image_src( get_post_thumbnail_id( $post_id ), 'product_library_image_size', true ); 
+			$product_posts['img'] = $custom_size_attachment_image[0];
+			$product_posts['title'] = get_the_title( $post_id ); 
+			$post_excerpt = apply_filters( 'the_excerpt', get_post_field('post_excerpt', $post_id) );
+			$product_posts['excerpt'] = ( !empty( $post_excerpt ) ) 
+			? $post_excerpt
+			: '';	
+	?>
+				<div class="grid-course pl-col-14 pl-3-col">
+					<div class="pl-border">
+						<div class="pl-box">
+							<?php if( $product_posts['img'] != '' ) : ?>
+							<div class="featured-image">
+								<a href="<?php echo $product_posts['link']; ?>">						
+									<img src="<?php echo $product_posts['img']; ?>" class="uo-grid-featured-image">						
+								</a>
+							</div>
+							<?php endif; ?>
+							<div class="course-info-holder">
+								<div class="course-title"><?php echo $product_posts['title']; ?></div>
+										<div class="content_desc"><?php echo wpautop( $product_posts['excerpt']); ?></div>
+							</div>
+							<a href="<?php echo $product_posts['link']; ?>"><div class="view-course-outline">View More</div></a>					
+							<!-- <div class="price-credit-btns">
+								<a href="<?php echo get_field( "sales_url", $p_id ); ?>">
+									<div class="view-course-outline" >Price: <?php echo get_field( "post_price", $p_id );  ?></div>
+								</a>	
+								<a href="<?php echo home_url('/credit-sale/'); ?>">
+									<div class="view-course-outline" >Credit: <?php echo get_field( "post_credit", $p_id );  ?></div>
+								</a>		
+							</div>		 -->				
+						</div>					
+					</div>
+				</div>	
+			<?php
+				endif;
+				endforeach;
+				wp_reset_postdata();
+			?>	
+			</div>
+
+	<?php elseif( $template_type == 3 ): ?>
+
+		
 			<p style="line-height: 50px; margin-bottom: 0px !important;" data-unit="px"><span class="tve_custom_font_size  rft" style="font-size: 30px;">
 				<font color="#ff6700">
 					<?php echo get_field( "title_top" ); ?>
@@ -22,15 +141,6 @@ get_header();
 				<hr class="tve_sep tve_sep1">
 			</div>
 	<?php
-	$category_id = ( get_field( 'category' ) != 0 ) ? get_field( 'category' ) : '-'.get_field( 'category' );
-
-	global $post;
-	$args = array (
-	    'cat' => array( $category_id ),
-	    'posts_per_page' => -1, //showposts is deprecated
-	    'fields' => 'ids'
-	);
-	$all_posts = get_posts( $args );
 
 	$accessible_posts = array();
 	$non_accessible_posts = array();
@@ -79,10 +189,18 @@ get_header();
 							</div>
 							<?php endif; ?>
 							<div class="course-info-holder">
-								<span class="course-title"><?php echo $accessible_post['title']; ?></span>
-										<p><?php echo $accessible_post['excerpt']; ?></p>	
+								<div class="course-title"><?php echo $accessible_post['title']; ?></div>
+										<div class="content_desc"><?php echo wpautop( $accessible_post['excerpt']); ?></div>
 							</div>
-							<a href="<?php echo $accessible_post['link']; ?>"><div class="view-course-outline" style="width: 100%;">View More</div></a>											
+							<a href="<?php echo $accessible_post['link']; ?>"><div class="view-course-outline">View More</div></a>					
+							<!-- <div class="price-credit-btns">
+								<a href="<?php echo get_field( "sales_url", $p_id ); ?>">
+									<div class="view-course-outline" >Price: <?php echo get_field( "post_price", $p_id );  ?></div>
+								</a>	
+								<a href="<?php echo home_url('/credit-sale/'); ?>">
+									<div class="view-course-outline" >Credit: <?php echo get_field( "post_credit", $p_id );  ?></div>
+								</a>		
+							</div>		 -->				
 						</div>					
 					</div>
 				</div>	
@@ -112,12 +230,21 @@ get_header();
 					<div class="pl-border">
 						<div class="pl-box">
 							<?php 
-								$view_more_link = $non_accessible_post['link'];
-								$credit_price = get_field( "credit_price", $p_id ); 
-								if( !empty( $credit_price) ) :
-									$view_more_link = home_url('/library-content-sales-page/'); 
+								// $view_more_link = $non_accessible_post['link'];
+								$required_credits = get_field( "post_credit", $p_id ); 
+
+								$user_credits = do_shortcode('[memb_contact fields=CLP_CREDITS]');
+								$user_credits = !empty( $user_credits ) ? $user_credits : 0 ;
+								$user_credits_class = ( $user_credits >= $required_credits ) ? 'show-credit-btn' : 'hide-credit-btn' ;
+
+								$access_tag_id = get_field( "memberium_tags", $p_id ); 
+
+								if( !empty( $required_credits) && $user_credits >= $required_credits ) :
+									$view_more_link = home_url( '/library-content-sales-page/?tag_id='.$access_tag_id.'&re_cr='.$required_credits ); 
+								else :
+									$view_more_link = 'javascript:void(null)'; 
 							?>
-							<div id="ribbon"><?php echo $credit_price; ?></div>
+							<!-- <div id="ribbon"><?php echo $credit_price; ?></div> -->
 							<?php endif; ?>
 							<?php if( $non_accessible_post['img'] != '' ) : ?>
 							<div class="featured-image">
@@ -127,10 +254,18 @@ get_header();
 							</div>
 							<?php endif; ?>
 							<div class="course-info-holder">
-								<span class="course-title"><?php echo $non_accessible_post['title']; ?></span>
-										<p><?php echo $non_accessible_post['excerpt']; ?></p>								
+								<div class="course-title"><?php echo $non_accessible_post['title']; ?></div>
+										<div class="content_desc"><?php echo wpautop( $non_accessible_post['excerpt']); ?></div>								
 							</div>
-							<a href="<?php echo $view_more_link; ?>"><div class="view-course-outline" style="width: 100%;">View More</div></a>											
+							<div class="price-credit-btns">
+								<a href="<?php echo get_field( "sales_url", $p_id ); ?>">
+									<div class="view-course-outline" >Price: <?php echo get_field( "post_price", $p_id );  ?></div>
+								</a>	
+
+								<a href="<?php echo $view_more_link ?>">
+									<div class="view-course-outline <?php echo $user_credits_class; ?>" >Credit: <?php echo $required_credits;  ?></div>
+								</a>		
+							</div>										
 						</div>					
 					</div>
 				</div>	
@@ -138,17 +273,55 @@ get_header();
 					endforeach;
 				?>	
 			</div>
-		</article>
+			<?php endif; ?>
+		</article>		
 	</div> <!-- main Content -->
 </div> <!-- Primary Content -->
 
 <style>
 /**/
+.content_desc{
+	display: block;
+
+	margin-top: 10px;
+}
+
+div.featured-image img.no-access {
+	 -webkit-filter: grayscale(100%);
+  -moz-filter: grayscale(100%);
+  -ms-filter: grayscale(100%);
+  -o-filter: grayscale(100%);
+  filter: grayscale(100%);
+  filter: gray; /* IE 6-9 */
+}
+
+div.featured-image img.grayscale { 
+	filter: url("data:image/svg+xml;utf8,<svg xmlns=\'http://www.w3.org/2000/svg\'><filter id=\'grayscale\'><feColorMatrix type=\'matrix\' values=\'0.3333 0.3333 0.3333 0 0 0.3333 0.3333 0.3333 0 0 0.3333 0.3333 0.3333 0 0 0 0 0 1 0\'/></filter></svg>#grayscale");
+	filter: gray;
+	-webkit-filter: grayscale(100%);
+}
 
 div.site-content article {
 	    padding: 20px 25px !important;
 }
+div.hide-credit-btn {
+	background: #c5c5c5 !important;
+    border: 1px solid #b90d25 !important;
+}
+div.hide-credit-btn:hover {
+	cursor: default;
+}
 
+.price-credit-btns a {
+    width: 49%;
+    display: inline-block;
+}
+.price-credit-btns a:last-child {
+     float: right;
+}
+.price-credit-btns a:first-child {
+     float: left;
+}
 .bottom-title {
 	display: block;
 	clear: left;
@@ -266,6 +439,7 @@ div.site-content article {
 	line-height: 1.2;
 	/*min-height: 60px;*/
 	padding: 10px;
+	min-height: 51px;
 	padding-bottom: 40px;
 	transition: all 0.75s ease;
 	-moz-transition: all 0.75s ease;
@@ -397,7 +571,7 @@ div.site-content article {
 }
 
 .pl-grid-wrapper .course-info-holder p {
-	margin-top: 10px;
+	/*margin-top: 10px;*/
 	font-size: 12px;
 	padding-right: 15px;
 	color: slategray;
@@ -440,7 +614,11 @@ div.site-content article {
 	.pl-grid-wrapper {
 		margin-right: -30px;
 	}
+	.pl-grid-wrapper .grid-course .course-info-holder{
+		min-height: 74px;
+	}
 }
+
 @media (min-width: 1330px) {
 	
 	.pl-3-col {
@@ -461,8 +639,8 @@ div.view-course-outline {
     /* white-space: nowrap; */
     padding: 5px 0px;
     text-transform: uppercase;
-    position: absolute;
-    bottom: 4px;
+    position: relative;
+
 }
 
 div.view-course-outline:hover {
@@ -474,11 +652,57 @@ div.view-course-outline:hover {
 /*}*/
 </style>
 <script>
-	jQuery(window).resize(function(){
-		jQuery('.view-course-outline').width( parseInt( jQuery('div.pl-box').width() - 2 ) );
-	});
-	jQuery(document).ready(function(){
-		jQuery('.view-course-outline').width( parseInt( jQuery('.grid-course').width() - 2 ) );
-	});
+	// Grayscale Images fix for IE10-IE11
+var GrayScaleFix = (function() {
+	var needToFix = /(MSIE 10)|(Trident.*rv:11\.0)|( Edge\/[\d\.]+$)/.test(navigator.userAgent);
+
+	function replaceImage(image) {
+		var tmpImage = new Image();
+		tmpImage.onload = function() {
+			var imgWrapper = document.createElement('span'),
+				svgTemplate = 
+				'<svg xmlns="http://www.w3.org/2000/svg" id="svgroot" viewBox="0 0 '+tmpImage.width+' '+tmpImage.height+'" width="100%" height="100%">' +
+					'<defs>' +
+					'<filter id="gray">' +
+						'<feColorMatrix type="matrix" values="0.3333 0.3333 0.3333 0 0 0.3333 0.3333 0.3333 0 0 0.3333 0.3333 0.3333 0 0 0 0 0 1 0" />' +
+					'</filter>' +
+					'</defs>' +
+					'<image filter="url(&quot;#gray&quot;)" x="0" y="0" width="'+tmpImage.width+'" height="'+tmpImage.height+'" preserveAspectRatio="none" xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="'+tmpImage.src+'" />' +
+				'</svg>';
+			
+			imgWrapper.innerHTML = svgTemplate;
+			imgWrapper.className = 'grayscale-fix';
+			image.parentNode.insertBefore(imgWrapper, image);
+
+			image.style.cssText += 'visibility:hidden;display:block';
+			imgWrapper.querySelector('svg').style.position = 'absolute';
+			imgWrapper.style.cssText = 'display:inline-block;position:relative;';
+			imgWrapper.appendChild(image);
+		};
+		tmpImage.src = image.src;
+	}
+
+	function replaceAll() {
+		var images = document.querySelectorAll('img.grayscale');
+		for(var i = 0; i < images.length; i++) {
+			replaceImage(images[i]);
+		}
+	}
+
+	if(needToFix) {
+		document.addEventListener('DOMContentLoaded', replaceAll);
+	}
+
+	return {
+		replace: replaceImage,
+		refresh: replaceAll
+	};
+}());
+	// jQuery(window).resize(function(){
+	// 	jQuery('.view-course-outline').width( parseInt( jQuery('div.pl-box').width() - 2 ) );
+	// });
+	// jQuery(document).ready(function(){
+	// 	jQuery('.view-course-outline').width( parseInt( jQuery('.grid-course').width() - 2 ) );
+	// });
 </script>
 <?php get_footer(); ?>
